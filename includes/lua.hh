8a3e8a891368ac2938f1a9b798bb286ab2719e48
Hugo Prenat <hugo.prenat@epitech.eu>
2015-05-07 14:34:59 +0200

    Just add the encapsulation of the include for lua in cpp

---
+++ includes/lua.hh
@@ -0,0 +1,18 @@
+//
+// lua.hh for Bomberman in /home/patoche/rendu/CPP/cpp_bomberman
+//
+// Made by Hugo Prenat
+// Login   <prenat_h@epitech.eu>
+//
+// Started on  Thu May  7 14:26:56 2015 Hugo Prenat
+// Last update Thu May  7 14:28:01 2015 Hugo Prenat
+//
+
+#ifndef LUA_HH_
+# define LUA_HH_
+
+extern "C" {
+#include <lua.h>
+}
+
+#endif // !LUA_HH_
