6acad0e4f144265922a1a6b190e3bf5408c41d7f
Milox Thomas <tommy@localhost.localdomain>
2015-05-07 08:31:27 +0200

    BombAERman:       class       Score:
                - upload best scores from .sore file.
                - formating date with current time
                - .score functional
                need to add more checks and talk with bros.

--- src/bomberman/Score.cpp
+++ src/bomberman/Score.cpp
@@ -5,7 +5,7 @@
 // Login   <milox_t@epitech.eu>
 //
 // Started on  Tue May  5 20:46:32 2015 TommyStarK
-// Last update Thu May  7 06:41:09 2015 TommyStarK
+// Last update Thu May  7 08:27:00 2015 TommyStarK
 //
 
 #include "Score.hpp"
@@ -13,25 +13,59 @@
 Score::Score()
   :_filename(".score")
 {
-  if (access(_filename.c_str(), F_OK) == -1)  {
-      _file.open(_filename.c_str(), std::fstream::in | std::fstream::out | std::fstream::app);
-      _file << "######################################" << std::endl;
-      _file << "##############BOMBAERMAN##############" << std::endl;
-      _file << "######################################" << std::endl;
-      _file << "##################BEST-SCORES#########" << std::endl;
-      _file << "######################################" << std::endl;
-      _file << "#####################copyright##2015##" << std::endl;
-  }
+  if (access(_filename.c_str(), F_OK) == -1)
+    this->updateScoreFile(false);
   else
-    this->show();
-
+    this->extractBestScores();
 }
-void                    Score::setStructScore(t_score *it, const std::string &tmp, int flag)
+
+Score::~Score()
 {
-  !flag ? it->player = tmp : it->date = tmp;
+  if (_file.is_open())
+    _file.close();
+  if (unlink(_filename.c_str()) == -1)
+    throw std::runtime_error("(Score::)~Score() - syscall unlink failed.");
+  this->updateScoreFile(true);
 }
 
-void                    Score::parse(t_score *n, const std::string &bestscore)
+
+/*
+** Parsing Tools:
+**    - epur string
+**    - get current time and format as follow year/month/day it into string
+**    - parser for operator <<
+**    - parser to get score from file
+*/
+
+std::string             Score::epur(const std::string &input)
+{
+  std::istringstream    i(input);
+  std::string           in;
+  std::string           final;
+
+  if (i >> in)
+    final += in;
+  while (i >> in)
+    final += ' ' + in;
+  return final;
+}
+
+std::string             Score::formatDate(struct tm *now, int flag)
+{
+  std::string           final;
+  std::ostringstream    convert;
+
+  if (!flag)
+    convert << (now->tm_year + 1900);
+  else if (flag == 1)
+    convert << (now->tm_mon + 1);
+  else
+    convert << (now->tm_mday);
+  final += convert.str();
+  return (final += flag < 2 ? "/" : "");
+}
+
+void                    Score::parseFromFile(t_score *n, const std::string &bestscore)
 {
   int                   score = 0;
   size_t                pos;
@@ -40,7 +74,7 @@ void                    Score::parse(t_score *n, const std::string &bestscore)
 
   while ((pos = handler.find(";")) != std::string::npos) {
     token = handler.substr(0, pos);
-    this->setStructScore(n, token, score);
+    !score ? n->player = token : n->date = token;
     handler.erase(0, pos + 1);
     ++score;
   }
@@ -50,25 +84,49 @@ void                    Score::parse(t_score *n, const std::string &bestscore)
   n->best = score;
 }
 
+void                    Score::parseNewScore(t_score *n, const std::string &bestscore)
+{
+  int                   score;
+  size_t                pos;
+  std::string           token;
+  std::string           handler = bestscore;
+  time_t                t = time(0);
+  struct tm             *now = localtime(&t);
+
+  while ((pos = handler.find(";")) != std::string::npos) {
+    token = handler.substr(0, pos);
+    n->player = token;
+    handler.erase(0, pos + 1);
+  }
+  token = handler.substr(0, handler.length());
+  std::stringstream    s(token);
+  s >> score;
+  n->best = score;
+  for (size_t i = 0; i < 3; ++i) {
+    n->date += formatDate(now, i);
+  }
+}
+
+
 /*
-** operator to add/modify best score
+** operator to add/modify best score formating as follow "PLAYERNAME;SCORE"
 */
 Score                   &Score::operator<<(const std::string &bestscore)
 {
   t_score               n;
 
-  this->parse(&n, bestscore);
+  this->parseNewScore(&n, bestscore);
   for (TOP::iterator i = _top.begin(); i != _top.end(); ++i)
   {
     if ((*i).player == n.player)
       {
         if (n.best > (*i).best)
           (*i).best = n.best;
-        return *this;
+        return (*this);
       }
   }
   _top.push_back(n);
-  return *this;
+  return (*this);
 }
 
 
@@ -88,8 +146,7 @@ void                    Score::orderTop()
     _top.clear();
     check = true;
   }
-  while (count)
-  {
+  while (count) {
     for (TOP::iterator i = tmp.begin(); i != tmp.end(); ++i) {
         (*i).best > ret ? ret = (*i).best : ret = ret;
     }
@@ -100,16 +157,61 @@ void                    Score::orderTop()
           (*i).best = -1;
       }
     }
-    --count;
-    this->orderTop();
+    return (--count, this->orderTop());
   }
   check = false;
-  return ;
 }
 
+/*
+** Create and Write new .score file for futures plays
+*/
+void                Score::updateScoreFile(bool end)
+{
+  std::string         tmp("");
 
+  _file.open(_filename.c_str(), std::fstream::in | std::fstream::out | std::fstream::app);
+  _file << "######################################" << std::endl;
+  _file << "##############BOMBAERMAN##############" << std::endl;
+  _file << "######################################" << std::endl;
+  _file << "##################BEST-SCORES#########" << std::endl;
+  _file << "######################################" << std::endl;
+  _file << "#####################copyright##2015##" << std::endl;
+  if (end)
+  {
+    this->orderTop();
+    for (TOP::iterator i = _top.begin(); i != _top.end(); ++i) {
+      std::ostringstream  convert;
 
+      convert << (*i).best;
+      tmp += (*i).player;
+      tmp += ";";
+      tmp += (*i).date;
+      tmp += ";";
+      tmp += convert.str();
+      _file << tmp << std::endl;
+      tmp.clear();
+    }
+  }
+}
 
+/*
+** get best scores from .score file and upload them into _top
+*/
+void                Score::extractBestScores()
+{
+  std::string       current;
+  std::ifstream     file(_filename.c_str());
+
+  while (std::getline(file, current))
+  {
+    if (current[0] != '#') {
+      t_score       n;
+      this->parseFromFile(&n, Score::epur(current));
+      _top.push_back(n);
+    }
+    current.clear();
+  }
+}
 
 
 /*
@@ -126,7 +228,6 @@ void                Score::show()
     tmp.clear();
   }
 }
-
 
 /*
 ** Display _top for debug
