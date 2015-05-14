2abf070b0db3727ee2d4dd2ab407de22e1e57c3a
Jean-Baptiste Grégoire <j.baptiste159@gmail.com>
2015-05-14 23:37:15 +0200

    Correct mistake on EMutex class

--- src/bomberman/EMutex.cpp
+++ src/bomberman/EMutex.cpp
@@ -1,11 +1,11 @@
 //
 // EMutex.cpp for Bomberman in /home/tran_0/rendu/cpp_bomberman
-// 
+//
 // Made by David Tran
 // Login   <tran_0@epitech.net>
-// 
+//
 // Started on  Thu May  7 18:44:46 2015 David Tran
-// Last update Thu May  7 18:54:21 2015 David Tran
+// Last update Thu May 14 23:36:52 2015 Jean-Baptiste Grégoire
 //
 
 #include "EMutex.hh"
@@ -32,7 +32,7 @@ int	EMutex::trylock()
 
 int	EMutex::unlock()
 {
-  return (pthread_mutex_trylock(&_mutex));
+  return (pthread_mutex_unlock(&_mutex));
 }
 
 pthread_mutex_t	*EMutex::getMutex()
