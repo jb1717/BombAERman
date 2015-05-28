15c8de505e3bbb9f8d317d62d5646d88c1e9f5c6
TommyStark <thomas.milox@epitech.eu>
2015-05-28 18:31:10 +0200

    [PlayUI] 	cleanup

--- src/bomberman/PlayUI.cpp
+++ src/bomberman/PlayUI.cpp
@@ -5,19 +5,41 @@
 // Login   <milox_t@epitech.eu>
 //
 // Started on  Sun May 24 17:22:00 2015 TommyStarK
-// Last update Sun May 24 18:02:08 2015 TommyStarK
+// Last update Thu May 28 13:59:22 2015 TommyStarK
 //
 
 #include "UIManager/PlayUI.hh"
 
 PlayUI::PlayUI()
 {
-
+  getIaNames();
 }
 
 PlayUI::~PlayUI()
 {
 
+}
+
+void                          PlayUI::getIaNames()
+{
+  DIR                         *dir = opendir("./src/ai");
+  size_t                      pos = 0;
+  struct dirent               *current = NULL;
+  std::vector<std::string>    tmp;
+
+  if (dir) {
+    while ((current = readdir(dir)) != NULL) {
+      tmp.push_back(current->d_name);
+    }
+    for (auto i : tmp) {
+      if ((pos = i.find_last_of(".")) != std::string::npos && i.substr(pos) == ".lua")
+      {
+        std::string token;
+        token = i.substr(0, pos);
+        _iaNames.push_back(token);
+      }
+    }
+  }
 }
 
 void                          PlayUI::launch()
